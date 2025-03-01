3d5ec6c273437555c3e32547973f3999613f18e6
lylebuist <102750153+lylebuist@users.noreply.github.com>
2025-03-01 18:41:11 +0000

    smaller ones

--- upscaling/upscaling-edsr.py
+++ upscaling/upscaling-edsr.py
@@ -7,6 +7,8 @@ from osgeo import gdal, ogr
 
 img=cv2.imread("C:\\Users\\lyleb\\Documents\\Uni Stuff\\4th Year\\ASR\\Hack-the-Bean\\upscaling\\satellite.jpg")
 
+img = img[:150, :150]
+
 sr = cv2.dnn_superres.DnnSuperResImpl_create()
  
 path = "C:\\Users\\lyleb\\Documents\\Uni Stuff\\4th Year\\ASR\\Hack-the-Bean\\upscaling\\EDSR_x4.pb"
