87136df11f9fe7c2935a3b707b1e514e41b80596
Adam Chapman <s2149975@ed.ac.uk>
2025-03-01 16:33:52 +0000

    image fudge removed

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,7 +1,5 @@
 import { Text, View } from "react-native";
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
-import ImageTest from "@/components/ImageTest";
-import TestParam from "@/components/TestParam";
 import Slider1 from "@/components/Slider1";
 export default function Index() {
   return (
@@ -12,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Slider1 testText = {"this is a test"} frontSource={'../assets/images/purple.png'} backSource={'../assets/images/back.png'}/>
+      <Slider1 testText = {"this is a test"} frontSource={'../assets/assets/images/purple.png'} backSource={'../assets/assets/images/back.png'}/>
     </View>
   );
 }
